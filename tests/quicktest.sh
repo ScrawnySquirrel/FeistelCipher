601796d93498ddbd8207eccf4395ee6c5ed3806c
Gabriel Lee <gabriellee@Gabriels-MacBook-Air.local>
2020-03-16 01:12:20 -0700

    Move test to folder

--- tests/quicktest.sh
+++ tests/quicktest.sh
@@ -9,9 +9,9 @@ ADDITIONAL_ARGS=${@}
 MODE="ecb"
 echo -e "\033[35mMode: ${MODE}\033[0m"
 echo -e "\033[96mEncryption...\033[0m"
-python3 feistel.py -e -i ${INPUT} -k foobar -o enc_out-ecb.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
+python3 -B feistel.py -e -i ${INPUT} -k foobar -o enc_out-ecb.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
 echo -e "\033[96mDecryption...\033[0m"
-python3 feistel.py -d -i enc_out-ecb.${EXT} -k foobar -o dec_out-ecb.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
+python3 -B feistel.py -d -i enc_out-ecb.${EXT} -k foobar -o dec_out-ecb.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
 if [[ "$EXT" == "bmp" ]]; then
   echo -e "\033[96mAttach BMP header...\033[0m"
   dd if=${INPUT} of=enc_out-ecb.${EXT} bs=1 count=54 conv=notrunc
@@ -23,9 +23,9 @@ echo ""
 MODE="cbc"
 echo -e "\033[35mMode: ${MODE}\033[0m"
 echo -e "\033[96mEncryption...\033[0m"
-python3 feistel.py -e -i ${INPUT} -k foobar -o enc_out-cbc.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
+python3 -B feistel.py -e -i ${INPUT} -k foobar -o enc_out-cbc.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
 echo -e "\033[96mDecryption...\033[0m"
-python3 feistel.py -d -i enc_out-cbc.${EXT} -k foobar -o dec_out-cbc.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
+python3 -B feistel.py -d -i enc_out-cbc.${EXT} -k foobar -o dec_out-cbc.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
 if [[ "$EXT" == "bmp" ]]; then
   echo -e "\033[96mAttach BMP header...\033[0m"
   dd if=${INPUT} of=enc_out-cbc.${EXT} bs=1 count=54 conv=notrunc
@@ -37,9 +37,9 @@ echo ""
 MODE="ctr"
 echo -e "\033[35mMode: ${MODE}\033[0m"
 echo -e "\033[96mEncryption...\033[0m"
-python3 feistel.py -e -i ${INPUT} -k foobar -o enc_out-ctr.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
+python3 -B feistel.py -e -i ${INPUT} -k foobar -o enc_out-ctr.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
 echo -e "\033[96mDecryption...\033[0m"
-python3 feistel.py -d -i enc_out-ctr.${EXT} -k foobar -o dec_out-ctr.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
+python3 -B feistel.py -d -i enc_out-ctr.${EXT} -k foobar -o dec_out-ctr.${EXT} -m ${MODE} ${ADDITIONAL_ARGS}
 if [[ "$EXT" == "bmp" ]]; then
   echo -e "\033[96mAttach BMP header...\033[0m"
   dd if=${INPUT} of=enc_out-ctr.${EXT} bs=1 count=54 conv=notrunc
